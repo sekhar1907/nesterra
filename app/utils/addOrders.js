f405ac90759f1b335b91bc93db8045bcb0950f9a
sekhar1907 <shekhar@aisoftwares.co.in>
2023-04-17 19:32:20 +0530

    17042023

---
+++ app/utils/addOrders.js
@@ -0,0 +1,8 @@
+export const serviceType = [
+    {label: 'Cable/Set', value: 'Cable/Set'},
+    {label: 'Data', value: 'Data'},
+    {label: 'Equipment', value: 'Equipment'},
+    {label: 'Voice', value: 'Voice'},
+    {label: 'Wireless', value: 'Wireless'},
+  ];
+  
