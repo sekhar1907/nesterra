fef21cfe5c2777545a251354a065b1b6202c7f50
sekhar1907 <shekhar@aisoftwares.co.in>
2023-05-24 19:12:20 +0530

    24052023

---
+++ app/screens/MapTypeAndFilter/Components/SiteCertified/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SiteCertified = () => {
+  return (
+    <View>
+      <Text>SiteCertified</Text>
+    </View>
+  )
+}
+
+export default SiteCertified
+
+const styles = StyleSheet.create({})
